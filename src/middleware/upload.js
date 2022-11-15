ef60efe54d644a153416e7431e3dd86f5e7b6b5b
Jacob Henriksen <Henriksen.Jacob@gmail.com>
2022-11-15 17:01:25 +0100

    first commit

---
+++ src/middleware/upload.js
@@ -0,0 +1,21 @@
+const util = require("util");
+const multer = require("multer");
+const maxSize = 2 * 2048 * 2048;
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __basedir + "/resources/static/assets/uploads/");
+  },
+  filename: (req, file, cb) => {
+    console.log(file.originalname);
+    cb(null, file.originalname);
+  },
+});
+
+let uploadFile = multer({
+  storage: storage,
+  limits: { fileSize: maxSize },
+}).single("file");
+
+let uploadFileMiddleware = util.promisify(uploadFile);
+module.exports = uploadFileMiddleware;
